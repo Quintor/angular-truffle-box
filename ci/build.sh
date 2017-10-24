19f14e9b72c95ab8fb29f0a20e2c556857e9c36c
Pim Otte <potte@quintor.nl>
2017-10-24 17:25:29 +0200

    Introducing Travis (no headless alterations yet)

---
+++ ci/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+truffle test
+ng test
+ng e2e
+ng lint
---
+++ ci/install-deps.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+
+npm install -g @angular/cli
+npm install -g truffle
+#npm install -g testrpc Unused in travis, for now
+npm install
