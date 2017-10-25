81578f0536e5877aabe232a66318e0695ef2332c
Pim Otte <potte@quintor.nl>
2017-10-25 08:12:30 +0200

    Testing if build fails if unit test fails

--- src/app/util/web3.service.spec.ts
+++ src/app/util/web3.service.spec.ts
@@ -27,6 +27,6 @@ describe('Web3Service', () => {
   });
 
   it('should be created', inject([Web3Service], (service: Web3Service) => {
-    expect(service).toBeTruthy();
+    expect(service).toBeFalsy();
   }));
 });
